1ee3fc7a5940512b617a82db66c6b0ce6aef2d7c
Steve Rubinstein <slrubinstein@gmail.com>
2014-06-05 18:09:18 -0400

    implement guessing feedback

--- js/app.js
+++ js/app.js
@@ -1,33 +1,87 @@
-// When document is ready, pick a random number between 1 and 100 and assign to variable "answer"
-
-//$("document").ready(function() {
-	var $answer = Math.ceil(Math.random()*100);
-	var guesses = [];
-//});
-
-// doc.ready created a local variable and I want a global variable, so for now removed doc.ready
+// Pick a random number between 1 and 100 and assign to variable "answer"
 
 
-// When guess is submitted, assign to variable "guess"
+var $answer = Math.ceil(Math.random()*100);
+var guesses = [];
+var $guess = $("#userGuess");
+var guessesRemaining = 5;
 
-$("form").submit(function(event) {
-	event.preventDefault()
-	var guess = $("#userGuess").val();
-	console.log("You guessed " + guess);
+
+$("span").prepend(guessesRemaining + ' ');
+
+// When guess button is clicked...
+$("button").click(function() {
+
+	//confirm guess is legal
+	if(!$guess.val()) {
+		return alert("Make a guess!");
+	}
+
+	if(isNaN($guess.val())) {
+		alert("That is not a number.");
+		return $guess.val('');
+	}
+
+	if($guess.val() < 0 || $guess.val() > 100) {
+		alert("Please enter a number between 1 and 100");
+		return $guess.val('');
+	}
+
+	//if guess is legal, assign it to a variable and push to array
+	var guess = $guess.val();
 	guesses.push(guess)
 
-// Check guess against answer
+	// Check guess against answer
+	//if guess == answer, you win
+	if(guess == $answer) {
+		return alert("You win!") // possibly create winEvent function
+	}
 
-if (guess == $answer) {
-	alert("You win!");
-} else {
-	alert("You guessed incorrectly");
-	displayGuesses(guesses);
+	//guesses remaining -= 1
+	guessesRemaining -= 1;
+	$("span").text(guessesRemaining + ' ');
+	$guess.val('');
+
+	//if guesses remaining == 0, you lose and button is disabled
+	if(guessesRemaining == 0) {
+		$("#temperature").text('');
+		$("span").prepend("<p>Sorry, you lose.<br>The number was " + $answer + "!</p>");
+		$("button").prop("disabled", true);
+	}	
+
+	//else give feedback based on guess
+	if(Math.abs(guess - $answer) < 10) {
+		$("#temperature").text("Hot!");
+		// change css color to reflect hotness
+	} else if (Math.abs(guess - $answer) < 20) {
+		$("#temperature").text("Warm");
+		// change css color to reflect hotness
+	} else if (Math.abs(guess - $answer) < 30) {
+		$("#temperature").text("Cool");
+		// change css color to reflect hotness
+	} else {
+		$("#temperature").text("Cold!");
+		// change css color to reflect hotness
 }
+
+	if(guess - $answer > 0) {
+		$("#highLow").text("Guess lower");
+	} else {
+		$("#highLow").text("Guess higher");
+	}
+
 
 });
 
-$("form .newGame").submit(function(event) {
+
+//hold list of past guesses
+
+
+
+
+
+
+$("button").submit(function(event) {
 	location.reload();
 });
 
